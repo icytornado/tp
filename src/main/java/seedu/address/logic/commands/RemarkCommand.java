03ac615170b5852eea44f2095cd1ee4c1fb51f0b
icytornado <perfecttzp@gmail.com>
2021-03-01 01:14:37 +0800

    final change

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -7,7 +7,6 @@ import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 import java.util.List;
 
 import seedu.address.commons.core.Messages;
-
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
@@ -90,5 +89,4 @@ public class RemarkCommand extends Command {
         return index.equals(e.index)
                 && remark.equals(e.remark);
     }
-
 }
