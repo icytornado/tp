a40e578d6f6bd80c158464f4820abc1ee8e0a667
icytornado <perfecttzp@gmail.com>
2021-03-09 13:43:21 +0800

    updatedoctor and getdoctor

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -48,7 +48,7 @@ public class PersonUtil {
     public static String getEditPersonDescriptorDetails(EditPersonDescriptor descriptor) {
         StringBuilder sb = new StringBuilder();
         descriptor.getName().ifPresent(name -> sb.append(PREFIX_NAME).append(name.fullName).append(" "));
-        descriptor.getName().ifPresent(doctor -> sb.append(PREFIX_DOCTOR).append(doctor.fullName).append(" "));
+        descriptor.getDoctor().ifPresent(doctor -> sb.append(PREFIX_DOCTOR).append(doctor.fullName).append(" "));
         descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_PHONE).append(phone.value).append(" "));
         descriptor.getEmail().ifPresent(email -> sb.append(PREFIX_EMAIL).append(email.value).append(" "));
         descriptor.getAddress().ifPresent(address -> sb.append(PREFIX_ADDRESS).append(address.value).append(" "));
